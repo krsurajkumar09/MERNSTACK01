117e9272aea5a350ae7e2071fffe006697e9a3a2
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2024-09-06 02:05:02 +0530

    Updating Auth.js miidleware

--- auth-mern-app/backend/middlewares/Auth.js
+++ auth-mern-app/backend/middlewares/Auth.js
@@ -1,20 +1,23 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+
 const ensureAuthenticated = (req, res, next) => {
-  const auth = req.headers["authorization"];
-  if (!auth) {
-    return res
-      .status(403)
-      .json({ message: "Unauthorized, JWT token is require" });
-  }
-  try {
-    const decoded = jwt.verify(auth, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res
-      .status(403)
-      .json({ message: "Unauthorized, JWT token wrong or expired" });
-  }
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Unauthorized, JWT token is required' });
+    }
+
+    const token = authHeader.split(' ')[1]; // Assuming the format is "Bearer <token>"
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized, JWT token is required' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized, JWT token is invalid or expired' });
+    }
 };
 
 module.exports = ensureAuthenticated;
